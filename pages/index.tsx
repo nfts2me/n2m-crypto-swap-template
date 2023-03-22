9ab5711b052b365e578ba0e9d1ad4aa445b620c3
NFTs2Me ⧫ NFT Toolkit for Creators <109629895+nfts2me@users.noreply.github.com>
2023-03-22 22:22:24 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,41 +14,6 @@ const Home: NextPage = () => {
 
         <main>
           <div>
-            <div className="container flex flex-col mx-auto">
-              <nav className="flex flex-row flex-wrap py-4 justify-between">
-                <div className="flex h-10">
-                  <a href="https://nfts2me.com" target="_blank" rel="noreferrer">
-                    <img src="https://nfts2me.com/assets/images/logo-text.svg" className="h-10" alt="nfts2me" />
-                  </a>
-                </div>
-                <div className="flex flex-row space-x-10 justify-around items-center">
-                  <a className="hover:text-red-500" href="https://nfts2me.com/">
-                    About Us
-                  </a>
-                  <a className="hover:text-red-500" href="https://github.com/nfts2me">
-                    How it works
-                  </a>
-                  <a className="hover:text-red-500" href="https://nfts2me.com/#why-choose-us">
-                    Features
-                  </a>
-                  <a className="hover:text-red-500" href="https://nfts2me.com/blog/">
-                    Blog
-                  </a>
-                  <a className="hover:text-red-500" href="https://docs.nfts2me.com/more/contact">
-                    Contact Us
-                  </a>
-                </div>
-                <div className="flex flex-row space-x-4">
-                  <div className="cursor-pointer flex flex-row rounded text-white bg-red-500 items-center justify-center px-2">
-                    <div className="flex flex-col justify-center">
-                      <p className="text-xs leading-3">Connect</p>
-                      <p className="text-xs leading-3">Your Wallet</p>
-                    </div>
-                  </div>
-
-                </div>
-              </nav>
-            </div>              
             <div className="py-16 bg-black">
               <iframe id='iframe-widget' src='https://tools.nfts2me.com/swap?widget=classic&bgColor=000000' style={{ height: "480px", width: "100%", border: "none" }}></iframe>
             </div>
